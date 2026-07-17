356afd1e7de0a550b3066b50c0a4eaa326549c36
Cleito004 <heitoravila375@gmail.com>
2026-07-17 15:57:52 -0300

    v4.2.3-17jul | Restore noseZDelta for Z-axis head tracking

--- script.js
+++ script.js
@@ -666,7 +666,8 @@ function runPrediction() {
       const bS = wS * 0.7 + hS * 0.3;
 
       const noseTipZ = nTip.z - nose.z;
-      const depAdj = clamp(noseTipZ * 0.06, -1, 3);
+      const noseZDelta = smooth.scanCompleted ? (nose.z - smooth.refNoseZ) : 0;
+      const depAdj = clamp(noseTipZ * 0.06 - noseZDelta * 0.04, -1, 3);
 
       const tPos = nose.clone()
         .addScaledVector(xAxis, sc.centerX + adjLateral)
